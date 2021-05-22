dacd5a7fcfa5ea87657e6cbd52cdb3a33e9ccd6a
hoholyin <holyin97@gmail.com>
2021-05-22 21:11:55 +0800

    complete contact bar

--- src/components/contactBar/webContactBar.js
+++ src/components/contactBar/webContactBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./webContactBar.css";
+import emailIcon from "../../images/email.png";
 
 const WebContactBar = (props) => {
     return (
@@ -11,6 +12,12 @@ const WebContactBar = (props) => {
                 <span className="web-contact-header-text">Contact Me</span>
                 <div className="web-contact-header-line-container">
                     <hr className="web-contact-header-line" />
+                </div>
+            </div>
+            <div className="web-email-container">
+                <img className="web-email-icon" src={emailIcon} alt="email icon"/>
+                <div className="web-email-address-container">
+                    <a href="mailto:holyin97@gmail.com" className="web-email">holyin97@gmail.com</a>
                 </div>
             </div>
         </div>
