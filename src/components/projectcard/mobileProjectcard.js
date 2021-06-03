f8da3c623973c38e35cfa18897bee5f3addf3c4a
hoholyin <holyin97@gmail.com>
2021-06-03 09:01:43 +0800

    allow mulitple languages

--- src/components/projectcard/mobileProjectcard.js
+++ src/components/projectcard/mobileProjectcard.js
@@ -3,14 +3,19 @@ import arrow from "../../images/arrow_forward.png";
 import {translateToTextLabel} from "../../commons/commons";
 
 const MobileProjectcard = (props) => {
-    const languageClassname = "language-container " + props.proj.language;
+    const languageColumnComponents = props.proj.languages.map((language) => {
+        const languageClassname = "language-container " + language;
+        return (
+            <div className="mobile-projectcard-language-container">
+                <div className={languageClassname}>{translateToTextLabel(language)}</div>
+            </div>
+        )
+    })
     return (
         <a className="mobile-projectcard-container" href={props.proj.link}>
             <img className="mobile-project-img" src={props.proj.img} alt="project image" />
             <span className="mobile-project-title">{props.proj.name}</span>
-            <div className="mobile-projectcard-language-container">
-                <div className={languageClassname}>{translateToTextLabel(props.proj.language)}</div>
-            </div>
+            {languageColumnComponents}
             <span className="mobile-project-desc">{props.proj.desc}</span>
             <div className="arrow-container">
                 <img className="mobile-project-arrow" src={arrow} alt="arrow" />
--- src/components/projectcard/webProjectcard.js
+++ src/components/projectcard/webProjectcard.js
@@ -3,15 +3,23 @@ import "./projectcard.css";
 import {translateToTextLabel} from "../../commons/commons";
 
 const WebProjectcard = (props) => {
-    const languageClassname = "language-container " + props.proj.language;
+    const languageRowComponents = props.proj.languages.map((language) => {
+        const languageClassname = "language-container " + language;
+        return (
+            <div className="web-projectcard-language-container">
+                <div className={languageClassname}>{translateToTextLabel(language)}</div>
+            </div>
+        )
+    })
+
     return (
         <a className="web-projectcard-container" href={props.proj.link} target="_blank">
             <img className="web-project-img" src={props.proj.img} alt="project image" />
             <div className="web-project-info-container">
                 <span className="web-project-title">{props.proj.name}</span>
                 <span className="web-project-desc">{props.proj.desc}</span>
-                <div className="web-projectcard-language-container">
-                    <div className={languageClassname}>{translateToTextLabel(props.proj.language)}</div>
+                <div className="web-language-row">
+                    {languageRowComponents}
                 </div>
             </div>
         </a>
