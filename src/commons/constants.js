99209d369d7a3b3f66af650809416690a048a9b0
hoholyin <holyin97@gmail.com>
2021-06-15 09:47:28 +0800

    update cap

--- src/commons/constants.js
+++ src/commons/constants.js
@@ -1,8 +1,6 @@
-
-
 export const nusName = "National University of Singapore";
 export const bCompInfo = "Bachelor of Computing (Honours)";
 export const bScInfo = "Bachelor of Science";
-export const bCompCap = "4.85/5.0";
-export const bScCap = "4.82/5.0";
+export const bCompCap = "4.86/5.0";
+export const bScCap = "4.81/5.0";
 
